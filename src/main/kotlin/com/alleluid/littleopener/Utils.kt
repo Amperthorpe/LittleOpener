8b9755c7ed25889101ccffb2b35af0d3becb4ccf
Alex Kaufman <milotig@gmail.com>
2019-05-22 17:23:48 -0700

    Add testing extension property

--- src/main/kotlin/com/alleluid/littleopener/Utils.kt
+++ src/main/kotlin/com/alleluid/littleopener/Utils.kt
@@ -1,6 +1,7 @@
 package com.alleluid.littleopener
 
 import net.minecraft.client.Minecraft
+import net.minecraft.util.math.BlockPos
 
 object Utils {
     const val validIntChars = "1234567890-"
@@ -16,5 +17,8 @@ object Utils {
             203,        //Left Arrow
             205,        //Right Arrow
             15          //Tab
-            )
+    )
+
+    val BlockPos.intMin
+        get() = BlockPos(Int.MIN_VALUE, Int.MIN_VALUE, Int.MIN_VALUE)
 }
