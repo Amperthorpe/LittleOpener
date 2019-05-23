5b22e3f15ed01a24a7c3abb6c08235b9d34614e2
Alex Kaufman <milotig@gmail.com>
2019-05-22 18:59:22 -0700

    Implement transfering coords into opener

--- src/main/kotlin/com/alleluid/littleopener/common/items/CoordChecker.kt
+++ src/main/kotlin/com/alleluid/littleopener/common/items/CoordChecker.kt
@@ -2,10 +2,13 @@ package com.alleluid.littleopener.common.items
 
 import com.alleluid.littleopener.LittleOpenerMod
 import com.alleluid.littleopener.MOD_ID
+import com.alleluid.littleopener.common.blocks.blockopener.BlockOpener
+import com.alleluid.littleopener.common.blocks.blockopener.TileOpener
 import net.minecraft.client.Minecraft
 import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.item.Item
+import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.util.EnumActionResult
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.EnumHand
@@ -26,6 +29,21 @@ object CoordChecker : Item(){
     override fun onItemUse(player: EntityPlayer, worldIn: World, pos: BlockPos, hand: EnumHand, facing: EnumFacing, hitX: Float, hitY: Float, hitZ: Float): EnumActionResult {
         if (worldIn.isRemote){
             Minecraft.getMinecraft().ingameGUI.addChatMessage(ChatType.SYSTEM, TextComponentString(pos.toString()))
+        }
+        val stack = player.getHeldItem(hand)
+        if (!stack.hasTagCompound()){
+            stack.tagCompound = NBTTagCompound()
+        }
+
+        if (worldIn.getBlockState(pos).block is BlockOpener){
+            val tileOpener = worldIn.getTileEntity(pos) as TileOpener
+            val longPos = stack.tagCompound!!.getLong("targetPos")
+            tileOpener.targetPos = BlockPos.fromLong(longPos)
+            if (worldIn.isRemote){
+                Minecraft.getMinecraft().ingameGUI.addChatMessage(ChatType.GAME_INFO, TextComponentString("Set opener to ${BlockPos.fromLong(longPos)}"))
+            }
+        } else {
+            stack.tagCompound!!.setLong("targetPos", pos.toLong())
         }
         return super.onItemUse(player, worldIn, pos, hand, facing, hitX, hitY, hitZ)
     }
