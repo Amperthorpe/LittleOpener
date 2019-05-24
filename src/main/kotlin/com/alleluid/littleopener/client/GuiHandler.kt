d5b7a2be3172d8057bf4080d964f4ffd8473803d
Alex Kaufman <milotig@gmail.com>
2019-05-23 22:34:34 -0700

    Remove unused files

--- src/main/kotlin/com/alleluid/littleopener/client/GuiHandler.kt
+++ src/main/kotlin/com/alleluid/littleopener/client/GuiHandler.kt
@@ -1,25 +1,22 @@
 package com.alleluid.littleopener.client
 
-import com.alleluid.littleopener.common.blocks.blockopener.ContainerOpener
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 import net.minecraftforge.fml.common.network.IGuiHandler
 
-enum class GuiID { LITTLE_OPENER, CONTAINER_OPENER }
+enum class GuiID { LITTLE_OPENER }
 
 class GuiHandler : IGuiHandler{
     override fun getClientGuiElement(ID: Int, player: EntityPlayer?, world: World?, x: Int, y: Int, z: Int): Any? {
         return when (GuiID.values()[ID]) {
             GuiID.LITTLE_OPENER -> GuiOpener(BlockPos(x, y, z))
-            GuiID.CONTAINER_OPENER -> GuiContainerOpener(getServerGuiElement(ID, player, world, x, y, z) as ContainerOpener, BlockPos(x, y, z))
         }
     }
 
     override fun getServerGuiElement(ID: Int, player: EntityPlayer?, world: World?, x: Int, y: Int, z: Int): Any? {
         return when (GuiID.values()[ID]) {
             GuiID.LITTLE_OPENER -> GuiOpener(BlockPos(x, y, z))
-            GuiID.CONTAINER_OPENER -> ContainerOpener(world!!, BlockPos(x, y, z))
         }
     }
 }
